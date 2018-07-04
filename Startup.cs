3b6860144cf2224d8c3ee0d37ac7cf25ebceb699
Hristo <hristobak@gmail.com>
2018-07-04 15:10:12 +0200

    Initial Solution Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plant_Smart.Startup))]
+namespace Plant_Smart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
